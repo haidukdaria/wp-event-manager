94137bef8a66c7fa9a14e9a06d2348aa7fea68cb
Daria Haiduk <haidukdari@gmail.com>
2023-07-10 16:54:13 +0300

    Add custom post type Event & price metabox

--- functions.php
+++ functions.php
@@ -17,3 +17,55 @@ function event_manager_setup() {
 	);
 }
 add_action( 'after_setup_theme', 'event_manager_setup' );
+
+//Create custom post type 
+function create_event_post_type() {
+  $labels = array(
+    'name' => 'Events',
+    'singular_name' => 'Event',
+  );
+
+  $args = array(
+    'labels' => $labels,
+    'public' => true,
+    'has_archive' => true,
+    'supports' => array('title', 'editor', 'thumbnail'),
+    'show_in_rest' => true,
+    'menu_icon' => 'dashicons-calendar',
+  );
+
+  register_post_type('event', $args);
+}
+add_action('init', 'create_event_post_type');
+
+// Add custom metabox to post type Event, 1/3
+function event_price_meta_box() {
+  add_meta_box(
+    'event_price',
+    'Price',
+    'event_price_meta_box_callback',
+    'event',
+    'normal',
+    'default'
+  );
+}
+add_action('add_meta_boxes', 'event_price_meta_box');
+
+// Show metabox, 2/3
+function event_price_meta_box_callback($post) {
+  $price = get_post_meta($post->ID, 'event_price', true); ?> 
+  <span>$</span>
+  <input type="text" name="event_price" value="<?php echo esc_attr($price); ?>"><?php
+}
+
+// Save metabox, 3/3
+function save_event_price_meta($post_id) {
+  if (array_key_exists('event_price', $_POST)) {
+    update_post_meta(
+      $post_id,
+      'event_price',
+      sanitize_text_field($_POST['event_price'])
+    );
+  }
+}
+add_action('save_post_event', 'save_event_price_meta');
